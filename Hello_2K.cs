9025d1e6972a7c457a20f5788fc2ca17a87fb497
Chintalapudi <surendra.chintalapudi@cgi.com>
2018-09-24 16:01:36 +0530

    Merge with Master from try2

--- Hello_2K.cs
+++ Hello_2K.cs
@@ -12,6 +12,7 @@ namespace myApp
                         Thread.Sleep(5000);
                         Console.WriteLine("Hello World!");
 			Console.WriteLine("Hello World!- This is my first commit");
+			Console.WriteLine("Hello World!- This is my first mdifications for Try1");
 			Console.WriteLine("Hello World!- This is my second modifications for try2");					
                 }
         }
